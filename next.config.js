d199f238d3ba4656cc88dcc6fa8556cb9c590746
NerdPen <83874226+NerdPen@users.noreply.github.com>
2021-07-22 12:01:51 +0800

    Update config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,8 @@ module.exports = {
     env: {
         PORT: 80,
         DOMAIN_PATH: "https://mile.cloud",
-        API_BASE_PATH: "https://uat-backend.mile.cloud",
+        API_BASE_PATH: "https://backend.mile.cloud",
         RECAPTCHAKEY: "6LdLAv8ZAAAAAOna-skW7yAI9CRfGgKwpWEUUWyC",
     },
 };
+å;
